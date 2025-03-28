a7c2e50c50c0f7a4ab6024092b7435f670e08cbe
Trey-Rachall <mathew.rachall@gmail.com>
2025-03-28 18:35:59 -0500

    finished solveBoardEfficient() and added documentation in sudoku.h

--- SudokuProject/include/sudoku.h
+++ SudokuProject/include/sudoku.h
@@ -38,17 +38,30 @@ bool solveBoard(int** BOARD, const int& r=0, const int& c=0);
 
 /**
   * @brief finds next cell with minimum amount of possibilities for number.
+  *
   * Takes a pointer to dynamically created 2D array (BOARD)
   *
   * @param BOARD board pointer to the dynamically allocated 2D array
-  * @return tuple of best cell with minimum solutions for cell
+  * @return tuple of cell with minimum solutions for cell
   */
 std::tuple<int, int, int> findNextCell(int** BOARD);
 
 
-  /**
-  * TODO: Provide appropriate Documentation, see other examples provided within the projects
-  */
+/**
+ * @brief Solves the Sudoku board using recursive backtracking.
+ *
+ * Uses findNextCell() to select the next empty cell with the fewest valid options.
+ * If no empty cells remain (r = -1, c = -1), the board is solved and returns true.
+ *
+ * Iterates through numbers 1 to 9, checking validity with isValid(). If valid, places
+ * the number and recursively calls solveBoardEfficient(). If the call returns true,
+ * the board is solved. If false, backtracks by resetting the cell to 0.
+ *
+ * Returns false if no valid number works.
+ *
+ * @param BOARD A pointer to the dynamically allocated 9x9 Sudoku board.
+ * @return true if the board is solved, false otherwise.
+ */
 bool solveBoardEfficient(int** BOARD);
 
 /**
